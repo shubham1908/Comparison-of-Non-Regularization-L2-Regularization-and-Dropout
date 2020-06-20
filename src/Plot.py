5de5e104aac8c80b9c5f96307cb281ec77a988e0
Shubham Gupta <46106892+shubham1908@users.noreply.github.com>
2020-06-20 11:04:07 +0530

    Create Plot.py

---
+++ src/Plot.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Functions import forward_propagation
+
+import operator
+from functools import reduce
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.maximum(0,Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = np.maximum(0,Z2)
+    Z3 = np.dot(W3, A2) + b3
+    A3 = 1/(1+np.exp(-Z3))
+    
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache
+
+
+def predict_dec(parameters, X):
+    a3, cache = forward_propagation(X, parameters)
+    predictions = (a3>0.5)
+    return predictions
+
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=reduce(operator.add,y), cmap=plt.cm.Spectral)
+    plt.show()
